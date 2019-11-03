1b2422a4d1801bcfb6568aec5054f93a38eee563
Md Mahbub Alam <39045006+Mahbub96@users.noreply.github.com>
2019-11-03 21:58:42 +0600

    Update assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -1,37 +1,46 @@
-#include <iostream>
+///Assignment on make forward and backward using STL
+#include<iostream>
 #include<stack>
-#include<stdlib.h>
-#include<conio.h>
 
 using namespace std;
-stack<string> forword;
-stack<string> backword;
-void fw(){
-    
-}
-void bw(){
-    
-}
-void url(){
-    cout<<endl;
-    string url;
-    cout<<"Enter URL: ";
-    cin>>url;
-    forword.push(url);
-}
-
-void menu(){
-    cout<<endl<<" 1. enter URL: \t 2.back \t 3.forword\t4.exit\n\t Press any key:\n";
-    int choice;
-    cin>>choice;
-    if(choice == 1) url();
-    else if(choice == 2 ) bw();
-    else if(choice ==3 ) fw();
-    else exit(1);
-    getch();
-    menu();
-}
 
 int main(){
-    menu();
+    stack<string> back_stack;
+    stack<string> fwd_stack;
+    string temp;
+
+    while(temp!="exit"){
+        cout<<"[1] visit URL \t[2] back [3] forward \t \n";
+        cin>>temp;
+        if(temp == "exit")
+            break;
+        else{
+            if(temp=="1"){
+            cout<<"Enter URL : ";
+            cin>>temp;
+            back_stack.push(temp);
+            while(fwd_stack.size()) fwd_stack.pop();
+        }
+        else if( temp == "2" ){
+            cout<<"\nGoing back...\n";
+            fwd_stack.push(back_stack.top());
+            back_stack.pop();
+        }
+        else if( temp == "3" ){
+            cout<<"\nGoing forward ..\n";
+
+            if(!fwd_stack.empty()){
+                back_stack.push(fwd_stack.top());
+                fwd_stack.pop();
+            }
+
+        }
+        if(back_stack.empty())
+            break;
+        cout<<"Current URL : "<<back_stack.top()<<endl;
+        }
+    }
+    return 0;
 }
+
+
