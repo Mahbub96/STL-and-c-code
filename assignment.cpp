28617835021a8fa8fe89f1e8b7e5600772965fc6
Md Mahbub Alam <39045006+Mahbub96@users.noreply.github.com>
2019-11-03 11:46:47 +0600

    Add files via upload

---
+++ assignment.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include<stack>
+
+#include<stdlib.h>
+
+#include<conio.h>
+
+using namespace std;
+stack<string> forword;
+stack<string> backword;
+void fw(){
+    
+}
+void bw(){
+    
+}
+void url(){
+    cout<<endl;
+    string url;
+    cout<<"Enter URL: ";
+    cin>>url;
+    forword.push(url);
+}
+
+void menu(){
+    cout<<endl<<" 1. enter URL: \t 2.back \t 3.forword\t4.exit\n\t Press any key:\n";
+    int choice;
+    cin>>choice;
+    if(choice == 1) url();
+    else if(choice == 2 ) bw();
+    else if(choice ==3 ) fw();
+    else exit(1);
+    getch();
+    menu();
+}
+
+int main(){
+    menu();
+}
