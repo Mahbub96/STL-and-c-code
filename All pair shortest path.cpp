9adafab4aca60a2da8adce6e1c2b95d0820986d0
Md Mahbub Alam <39045006+Mahbub96@users.noreply.github.com>
2019-11-13 09:23:28 +0600

    Final version

--- All pair shortest path.cpp
+++ All pair shortest path.cpp
@@ -5,36 +5,49 @@
 using namespace std;
 
 int main(){
-    freopen("input.txt","r",stdin);
+    //freopen("input.txt","r",stdin);
     int adj[100][100];
     int i,j,k;
     int node,m,n,val;
-    //cout<<"Enter Nodes Number:";
+    cout<<"Enter Nodes Number:";
     cin>>node;
-    for( i = 0; i < node; i++ ){
-        for( j = 0; j < node; j++ ){
-            //cout<<"Enter data for ["<<i<<"] ["<<j<<"] :";
+    for( i = 1; i <= node; i++ ){
+        for( j = 1; j <= node; j++ ){
+            cout<<"Enter Adj Matrix data for ["<<i<<"] ["<<j<<"] :";
             cin>>adj[i][j];
         }
     }
 
-    for( k = 1; k <= node; k++ ){
-        printf("\n\nD[%d] no Matrix :\n",k);
-        for(i = 0; i < node; i++){
-            for( j = 0; j < node; j++ ){
-                if((i==k) || (j==k)){
-                    if( adj[i][j]>60 ) printf("%2c*\t",236);
-                    else printf("%2d*\t",adj[i][j]);
+    for( k = 0; k <= node; k++ ){
+        if(!k){
+            printf("\n\nD[%d] no Matrix :\n",k);
+            for( i = 1; i <= node; i++ ){
+                for( j = 1; j <= node; j++ ){
+                    if( adj[i][j]>60 ) printf("%2c\t",236);
+                    else printf("%2d\t",adj[i][j]);
                 }
-                else{
-                    m = adj[i][j];
-                    n = adj[i][k] + adj[k][j];
-                    val = min(m,n);
-                    if( val>60 ) printf("%2c\t",236);
-                    else printf("%2d\t",val);
-                }
+                cout<<endl;
             }
-            cout<<endl;
+        }
+        else{
+            printf("\n\nD[%d] no Matrix :\n",k);
+            for(i = 1; i <= node; i++){
+                for( j = 1; j <= node; j++ ){
+                    if((i==k) || (j==k)){
+                        if( adj[i][j]>60 ) printf("%2c\t",236);
+                        else printf("%2d\t",adj[i][j]);
+                    }
+                    else{
+                        m = adj[i][j];
+                        n = adj[i][k] + adj[k][j];
+                        val = min(m,n);
+                        adj[i][j] = val;
+                        if( adj[i][j]>60 ) printf("%2c\t",236);
+                        else printf("%2d\t",adj[i][j]);
+                    }
+                }
+                cout<<endl;
+            }
         }
     }
     return 0;
